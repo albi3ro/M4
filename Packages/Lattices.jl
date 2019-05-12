a5e442305b886d63874d2ff813edd60a0391c72e
Christina Lee <chrissie.c.l@gmail.com>
2019-05-12 15:36:50 -0700

    updating to v1.0

--- Packages/Lattices.jl
+++ Packages/Lattices.jl
@@ -1,7 +1,7 @@
 module Lattices
 export Lattice, MakeLattice
 
-    type Lattice
+    struct Lattice
         name::String
         l::Int16
         dim::Int8
@@ -39,43 +39,43 @@ export Lattice, MakeLattice
     end
 
     function MakeSquare(l)
-          N=l^2;
-          d=2;
-          X=Array{Int16}(N,2);
-          nnei=4;
-          neigh=Array{Int16}(N,4);
+        N=l^2;
+        d=2;
+        X=Array{Int16}(undef,N,2);
+        nnei=4;
+        neigh=Array{Int16}(undef,N,4);
 
-          X[:,1]=armod(collect(1:N),l);
-          X[:,2]=ceil.(collect(1:(N))/l);
-          a=[[1 0]
-              [0 1]];
-          unit=[0,0];
+        X[:,1]=armod.(collect(1:N),l);
+        X[:,2]=ceil.(collect(1:(N))/l);
+        a=[[1 0]
+            [0 1]];
+        unit=[0,0];
 
-          neigh[:,1]=armod(X[:,1]+1,l)+l*(X[:,2]-1);
-          neigh[:,2]=armod(X[:,1]+l-1,l)+l*(X[:,2]-1);
-          neigh[:,3]=X[:,1]+mod.(X[:,2],l)*l;
-          neigh[:,4]=X[:,1]+mod.(X[:,2]+l-2,l)*l;
+        neigh[:,1]=armod.(X[:,1].+1,l).+l*(X[:,2].-1);
+        neigh[:,2]=armod.(X[:,1].+l.-1,l).+l*(X[:,2].-1);
+        neigh[:,3]=X[:,1].+mod.(X[:,2],l)*l;
+        neigh[:,4]=X[:,1].+mod.(X[:,2].+l.-2,l)*l;
 
-          return Lattice("Square",l,d,a,unit,N,X,nnei,neigh)
+        return Lattice("Square",l,d,a,unit,N,X,nnei,neigh)
       end
 
       function MakeCheckerboard(l)
           N=l^2;
           d=2;
-          X=Array{Int16}(N,2);
+          X=Array{Int16}(undef,N,2);
           nnei=6;
-          neigh=Array{Int16}(N,6);
+          neigh=Array{Int16}(undef,N,6);
 
-          X[:,1]=armod(collect(1:N),l);
+          X[:,1]=armod.(collect(1:N),l);
           X[:,2]=ceil.(collect(1:(N))/l);
           a=[[1 0]
               [0 1]];
           unit=[0,0];
 
-          neigh[:,1]=armod(X[:,1]+1,l)+l*(X[:,2]-1);
-          neigh[:,2]=armod(X[:,1]+l-1,l)+l*(X[:,2]-1);
-          neigh[:,3]=X[:,1]+mod.(X[:,2],l)*l;
-          neigh[:,4]=X[:,1]+mod.(X[:,2]+l-2,l)*l;
+          neigh[:,1]=armod.(X[:,1].+1,l).+l*(X[:,2].-1);
+          neigh[:,2]=armod.(X[:,1].+l.-1,l).+l*(X[:,2].-1);
+          neigh[:,3]=X[:,1].+mod.(X[:,2],l)*l;
+          neigh[:,4]=X[:,1].+mod.(X[:,2].+l.-2,l)*l;
 
           for ii in 1:N
               if isodd(X[ii,1])==isodd(X[ii,2])
@@ -94,14 +94,14 @@ export Lattice, MakeLattice
         N=l
         d=1
         nnei=2
-        neigh=Array{Int16}(N,2)
+        neigh=Array{Int16}(undef,N,2)
         a=[1];
         unit=[0];
 
         X=collect(1:l);
 
-        neigh[:,1]=armod(collect(2:(l+1)),l);
-        neigh[:,2]=armod(collect(l:(2*l-1)),l);
+        neigh[:,1]=armod.(collect(2:(l+1)),l);
+        neigh[:,2]=armod.(collect(l:(2*l-1)),l);
 
         return Lattice("Chain",l,d,a,unit,N,X,nnei,neigh)
     end
@@ -122,10 +122,10 @@ export Lattice, MakeLattice
         ncell=size(a)[1];
         N=l*l*ncell;
 
-        a1=repeat(a[1,:],outer=[ncell,1]);
-        a2=repeat(a[2,:],outer=[ncell*l,1]);
+        a1=repeat(transpose(a[1,:]),outer=[ncell,1]);
+        a2=repeat(transpose(a[2,:]),outer=[ncell*l,1]);
 
-        X=Array{Float64}(N,2);
+        X=Array{Float64}(undef,N,2);
         # Here we are actually calculating the positions for every site
         for i in 1:l    #for the first row
             X[ncell*i-ncell+1:ncell*i,:]=unit+(i-1)*a1;
@@ -135,10 +135,9 @@ export Lattice, MakeLattice
             X[(ncell*l*(j-1)+1):(ncell*l*(j-1)+ncell*l),:]=X[1:ncell*l,:]+(j-1)*a2;
         end
 
-        neigh=Array{Int16}(N,nnei)
+        neigh=Array{Int16}(undef,N,nnei)
 
         return Lattice(name,l,d,a,unit,N,X,nnei,neigh)
     end
-
 
 end
